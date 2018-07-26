8d0e7d83299a1409ddcb23288832b6963b32f371
mhkeller <code@mhkeller.com>
2018-07-26 01:04:52 -0400

    init

---
+++ README.md
@@ -0,0 +1,13 @@
+LayerCake Examples
+===
+
+> graphics framework for [sveltejs](https://github.com/sveltejs/svelte)
+
+_Looking for the library? See <https://github.com/mhkeller/layercake>!_
+
+```sh
+git clone https://github.com/mhkeller/layercake-examples.git && cd layercake-examples
+npm install && npm start
+```
+
+Open <http://localhost:8000>
---
+++ src/main.js
@@ -0,0 +1,26 @@
+import LayerCake from 'layercake';
+import points from './data/points.js';
+
+import Line from './components/Line.html';
+import Area from './components/Area.html';
+import AxisX from './components/AxisX.html';
+import AxisY from './components/AxisY.html';
+
+const myCake = new LayerCake({
+	padding: { top: 0, right: 0, bottom: 20, left: 25 },
+	x: 'myX',
+	y: d => d['myY'],
+	yDomain: [0, null],
+	data: points,
+	target: document.getElementById('chart-target')
+})
+	.svgLayers([
+		{ component: AxisX, opts: {} },
+		{ component: AxisY, opts: {} },
+		{ component: Line, opts: {} },
+		{ component: Area, opts: {} }
+	]);
+
+console.log(myCake.get());
+
+myCake.render();
